903139dd83357939f5da5cad758e823a6d13278e
Maksim <msk42@tpu.ru>
2023-10-07 02:28:53 +0700

    add MainWindow class

--- tutorials/first_app.py
+++ tutorials/first_app.py
@@ -1,9 +1,21 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 # import sys
 
 app = QApplication([])
 
-window = QWidget()
+# window = QWidget() 1
+# window = QPushButton("Push me!")
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("This is my app")
+        button = QPushButton("Press Me")
+
+        self.setCentralWidget(button)
+
+window = MainWindow()
 window.show()
 
 app.exec()
