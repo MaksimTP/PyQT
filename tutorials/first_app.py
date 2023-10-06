45f231bc6454f84a5a2a0e16d4d8c2d8d709cc4c
Maksim <msk42@tpu.ru>
2023-10-07 02:43:34 +0700

    Add signals

--- tutorials/first_app.py
+++ tutorials/first_app.py
@@ -1,19 +1,24 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+from PyQt6.QtCore import Qt ,QSize
 # import sys
 
 app = QApplication([])
 
-# window = QWidget() 1
-# window = QPushButton("Push me!")
-
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super(MainWindow, self).__init__()
 
-        self.setWindowTitle("This is my app")
-        button = QPushButton("Press Me")
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.btn_was_clckd)
 
         self.setCentralWidget(button)
+
+    def btn_was_clckd(self):
+        print("Clicked!")
+
 
 window = MainWindow()
 window.show()
