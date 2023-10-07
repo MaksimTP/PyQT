c692fa3db748b86eb26e59788a98d13732ed8a40
Maksim <msk42@tpu.ru>
2023-10-08 02:14:19 +0700

    Add files using Qt Designer

---
+++ tutorials/8_10_23.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QDialog
+from ui_imagedialog import Ui_Dialog
+
+app = QApplication(sys.argv)
+window = QDialog()
+ui = Ui_Dialog()
+ui.setupUi(window)
+
+window.show()
+
+sys.exit(app.exec())
